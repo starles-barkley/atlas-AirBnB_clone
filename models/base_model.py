e6121b4d6474ac571b2c512403cef846a7a7442f
grahacr <courtney.graham@atlasschool.com>
2024-02-01 15:47:59 -0600

    update init method prototype

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@ import uuid
 import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, id, created_at, updated_at):
         self.id = str(uuid.uuid4())
         self.created_at: datetime.datetime.now()
         self.updated_at: datetime.datetime.now()
