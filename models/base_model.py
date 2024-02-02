afb7eda22a1e979a75aa6b4ab38f30ac6d50914f
grahacr <courtney.graham@atlasschool.com>
2024-02-02 15:24:23 -0600

    main test file and update init method with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,19 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = str(uuid.uuid4())
+                elif key == 'created_at':
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.fromisoformat(value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
