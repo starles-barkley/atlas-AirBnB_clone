47e5464aa8b2e161720e475d2227a5f5fe3efabb
grahacr <courtney.graham@atlasschool.com>
2024-02-02 19:22:45 +0000

    change import, change the calling of name within str method

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        self.class_name = self.__class__.__name__
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
-            self.class_name, self.id, self.__dict__)
+            type(self.name), self.id, self.__dict__)
 
     def save(self):
         return self.updated_at
 
-    def to_dict(self):
-        
+    def to_dict(self): 
         dummy_dict = {
-            self.__class__.__name__: {
-                "id": self.id,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat(),
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
             }
-        }
         return dummy_dict
