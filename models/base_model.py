eac82e344ddfbff7f7b40249fd94547a2a6c1c17
grahacr <courtney.graham@atlasschool.com>
2024-02-05 22:42:02 +0000

    fix base model typo, start work on console

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """ conductor method for initializing instances """
+        from models import storage
         formatted = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key in kwargs:
@@ -27,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ print string representation of created object """
@@ -34,7 +36,8 @@ class BaseModel:
             type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        return self.updated_at
+        from models import storage
+        storage.save()
 
     def to_dict(self): 
         obj_dict = self.__dict__.copy()
