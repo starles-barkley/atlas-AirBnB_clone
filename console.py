61d7dd8d20e57c2a4936db7ab9d1c2e24fe42171
grahacr <courtney.graham@atlasschool.com>
2024-02-05 22:56:31 -0600

    add do create method in console - tested and works

--- console.py
+++ console.py
@@ -2,13 +2,14 @@
 """ module for python command line interpreter """
 import cmd
 import sys
+import importlib
 from models.base_model import BaseModel
 from models.__init__ import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """ class initiates command loop"""
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """ quit method quits interpreter """
@@ -19,9 +20,30 @@ class HBNBCommand(cmd.Cmd):
             print()
             exit()
 
+    def emptyline(self):
+         pass
+
     def help_quit(self):
         """ help message for quit method"""
         print("Exit the interpreter")
+
+    def do_create(self, arg):
+        args = arg.split()
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        try:
+            module = importlib.import_module('models.base_model')
+            class_ = getattr(module, class_name)
+            instance = class_()
+            instance.save()
+            print(instance.id)
+        except AttributeError:
+             print("** class doesn't exist **")
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
